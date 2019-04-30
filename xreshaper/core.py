fa956a5b01cce9e228a5cbd6d9ae6e9bf42af850
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-30 10:39:24 -0600

    Formatting only

--- xreshaper/core.py
+++ xreshaper/core.py
@@ -6,19 +6,19 @@ import xarray as xr
 
 def read_time_slices(input_directory=None):
     if input_directory:
-        paths = f"{input_directory}/*.nc"
-        dset = xr.open_mfdataset(paths, data_vars="minimal")
+        paths = f'{input_directory}/*.nc'
+        dset = xr.open_mfdataset(paths, data_vars='minimal')
         return dset
 
     else:
-        raise ValueError(f"input_directory must be specified.")
+        raise ValueError(f'input_directory must be specified.')
 
 
 def find_time_dependent_vars(dset=None):
     if not isinstance(dset, xr.Dataset):
         raise TypeError(
-            f"dataset must be of type={xr.Dataset}.\
-                         The function was called with dset={dset}"
+            f'dataset must be of type={xr.Dataset}.\
+                         The function was called with dset={dset}'
         )
 
     else:
@@ -27,7 +27,7 @@ def find_time_dependent_vars(dset=None):
         metadata_vars = []
 
         for var in all_variables:
-            if "time" in dset.data_vars[var].dims:
+            if 'time' in dset.data_vars[var].dims:
                 tseries_vars.append(var)
             else:
                 metadata_vars.append(var)
@@ -38,8 +38,8 @@ def find_time_dependent_vars(dset=None):
 def create_time_series(output_directory, output_prefix, output_suffix, dset=None):
     if not isinstance(dset, xr.Dataset):
         raise TypeError(
-            f"dataset must be of type={xr.Dataset}.\
-                         The function was called with dset={dset}"
+            f'dataset must be of type={xr.Dataset}.\
+                         The function was called with dset={dset}'
         )
 
     else:
@@ -58,21 +58,19 @@ def create_time_series(output_directory, output_prefix, output_suffix, dset=None
 
             # Add dataset global attributes to time series variable dataset
             var_dset.attrs = dset.attrs
-            fpath = f"{output_directory}/{output_prefix}{vname}{output_suffix}"
+            fpath = f'{output_directory}/{output_prefix}{vname}{output_suffix}'
             datasets.append(var_dset)
             paths.append(fpath)
         print(
-            f"*** The generated time series files are located \
-            in : {output_directory} ***"
+            f'*** The generated time series files are located \
+            in : {output_directory} ***'
         )
     return datasets, paths
 
 
 def save_to_disk(datasets, paths, engine):
     if not isinstance(datasets, list) or not isinstance(paths, list):
-        raise TypeError(
-            f"datasets and paths must be specified and must be of type={list}"
-        )
+        raise TypeError(f'datasets and paths must be specified and must be of type={list}')
 
     else:
-        xr.save_mfdataset(datasets, paths, mode="w", engine=engine)
+        xr.save_mfdataset(datasets, paths, mode='w', engine=engine)
